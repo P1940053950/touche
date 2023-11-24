94ad086992251b697a27521dfd8f5e465e25ba27
Lera <mssolipsismus@gmail.com>
2023-11-24 19:58:40 +0100

    change patient object

---
+++ backend/src/main/java/com/touche/backend/domain/CancerType.java
@@ -0,0 +1,16 @@
+package com.touche.backend.domain;
+
+public enum CancerType {
+
+        CRANIOSPINAL,
+        BREAST,
+        BREAST_SPECIAL("BREAST SPECIAL"),
+        HEAD_AND_NECK("HEAD & NECK"),
+        ABDOMEN("ABDOMEN"),
+        PELVIS("PELVIS"),
+        CRANE("CRANE"),
+        LUNG("LUNG"),
+        LUNG_SPECIAL("LUNG SPECIAL"),
+        WHOLE_BRAIN("WHOLE BRAIN");
+
+}
--- backend/src/main/java/com/touche/backend/domain/Patient.java
+++ backend/src/main/java/com/touche/backend/domain/Patient.java
@@ -15,6 +15,7 @@ import javax.persistence.Column;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
+import java.util.Date;
 
 
 @Getter
@@ -25,28 +26,6 @@ import java.time.Instant;
 @Table(name = "patients")
 public class Patient {
 
-    public enum CancerType {
-        CRANIOSPINAL("CRANIOSPINAL"),
-        BREAST("BREAST"),
-        BREAST_SPECIAL("BREAST SPECIAL"),
-        HEAD_AND_NECK("HEAD & NECK"),
-        ABDOMEN("ABDOMEN"),
-        PELVIS("PELVIS"),
-        CRANE("CRANE"),
-        LUNG("LUNG"),
-        LUNG_SPECIAL("LUNG SPECIAL"),
-        WHOLE_BRAIN("WHOLE BRAIN");
-
-        private final String value;
-
-        CancerType(String value) {
-            this.value = value;
-        }
-
-        public String getValue() {
-            return value;
-        }
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -54,20 +33,18 @@ public class Patient {
 
     @NonNull
     @NotBlank
-    private String firstName;
-
-    @NonNull
-    @NotBlank
-    private String lastName;
+    private String name;
 
     @NonNull
     @NotBlank
     @Email
-    @Column(unique = true)
     private String email;
 
-    @CreationTimestamp
-    private Instant createdAt;
+    @NonNull
+    @NotBlank
+    private String phone;
+
+    private Date createdAt;
 
     @Enumerated(EnumType.STRING)
     private CancerType cancerType;
