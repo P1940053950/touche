ccb05ffc6df86ff714b585088f4241756d0a56f4
Lera <mssolipsismus@gmail.com>
2023-11-24 21:21:57 +0100

    add repository

--- backend/src/main/java/com/touche/backend/repository/AppointmentRepository.java
+++ backend/src/main/java/com/touche/backend/repository/AppointmentRepository.java
@@ -1,4 +1,14 @@
-//package com.touche.backend.repository;
-//
-//public interface AppointmentRepository extends JpaRepository<> {
-//}
+package com.touche.backend.repository;
+
+import com.touche.backend.domain.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    List<Appointment> getAllById();
+
+    Optional<Appointment> getAppointmentById(long id);
+}
--- backend/src/main/java/com/touche/backend/repository/MachineRepository.java
+++ backend/src/main/java/com/touche/backend/repository/MachineRepository.java
@@ -1,4 +1,13 @@
-//package com.touche.backend.repository;
-//
-//public interface MachineRepository extends JpaRepository<> {
-//}
+package com.touche.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.touche.backend.domain.Machine;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MachineRepository extends JpaRepository<Machine, Long> {
+
+    List<Machine> findAllById();
+    Optional<Machine> findById(long id);
+}
