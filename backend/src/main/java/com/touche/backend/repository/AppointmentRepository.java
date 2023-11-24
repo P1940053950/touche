76a8f5f956696eb7cb0af8cd16e8b8a604602922
Lera <mssolipsismus@gmail.com>
2023-11-24 23:12:52 +0100

    Merge branch 'main' of https://github.com/P1940053950/touche

--- backend/src/main/java/com/touche/backend/repository/AppointmentRepository.java
+++ backend/src/main/java/com/touche/backend/repository/AppointmentRepository.java
@@ -2,13 +2,15 @@ package com.touche.backend.repository;
 
 import com.touche.backend.domain.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
-    List<Appointment> getAllById();
-
-    Optional<Appointment> getAppointmentById(long id);
+//    List<Appointment> getAllById();
+//
+//    Optional<Appointment> getAppointmentById(long id);
 }
--- backend/src/main/java/com/touche/backend/repository/MachineRepository.java
+++ backend/src/main/java/com/touche/backend/repository/MachineRepository.java
@@ -2,12 +2,14 @@ package com.touche.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.touche.backend.domain.Machine;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MachineRepository extends JpaRepository<Machine, Long> {
 
-    List<Machine> findAllById();
-    Optional<Machine> findById(long id);
+//    List<Machine> findAllById();
+//    Optional<Machine> findById(long id);
 }
