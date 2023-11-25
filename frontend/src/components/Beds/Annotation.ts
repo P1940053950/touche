ab6ebc20551bcb782a39ea1ee19292bbb0a3f223
Szloboda Gergo <szloboda.gergo@itqs.hu>
2023-11-25 20:32:59 +0100

    frontend

--- frontend/src/components/Beds/Annotation.ts
+++ frontend/src/components/Beds/Annotation.ts
@@ -1,6 +1,7 @@
 import { AnnotationData } from './types';
 
 export const annotations: AnnotationData[] = [
+<<<<<<< Updated upstream
   {
     x: 82,
     y: 153,
@@ -91,6 +92,17 @@ export const annotations: AnnotationData[] = [
     id: 14,
     users: ['User1', 'User2', 'User3', 'User4', 'User5'],
   },
+=======
+    { x: 0.05172862988585325, y: 0.11828817234109153, width: 0.08142508142508142, height: 0.11599931678486997, occupiedPlaces: 3, freePlaces: 1, id: 87, usernames: ["User1", "User2", "User3"] },
+    { x: 0.13849610336347102, y: 0.11828817234109153, width: 0.07578875171467764, height: 0.11599931678486997, occupiedPlaces: 5, freePlaces: 2, id: 46, usernames: ["User1", "User2", "User3", "User4", "User5"] },
+    { x: 0.21949610336347103, y: 0.12009534452554744, width: 0.07894736842105263, height: 0.11421445021445021, occupiedPlaces: 1, freePlaces: 3, id: 73, usernames: ["User1"] },
+    { x: 0.698223345497138, y: 0.1245124568210744, width: 0.07578875171467764, height: 0.11242958364403044, occupiedPlaces: 4, freePlaces: 3, id: 25, usernames: ["User1", "User2", "User3", "User4"] },
+    { x: 0.8093681917211329, y: 0.1227052846366185, width: 0.07894736842105263, height: 0.11242958364403044, occupiedPlaces: 1, freePlaces: 3, id: 32, usernames: ["User1"] },
+    { x: 0.11817288374178177, y: 0.5382924008220616, width: 0.07518807518807519, height: 0.06135504034838006, occupiedPlaces: 2, freePlaces: 2, id: 45, usernames: ["User1", "User2"] },
+    { x: 0.15769507803173424, y: 0.5382924008220616, width: 0.07627118644067797, height: 0.06135504034838006, occupiedPlaces: 3, freePlaces: 2, id: 79, usernames: ["User1", "User2", "User3"] },
+    { x: 0.5575170842824602, y: 0.5382924008220616, width: 0.07627118644067797, height: 0.06135504034838006, occupiedPlaces: 2, freePlaces: 2, id: 36, usernames: ["User1", "User2"] },
+    { x: 0.6423278608393295, y: 0.5382924008220616, width: 0.08142508142508142, height: 0.06135504034838006, occupiedPlaces: 5, freePlaces: 0, id: 14, usernames: ["User1", "User2", "User3", "User4", "User5"] }
+>>>>>>> Stashed changes
 ];
 
 export default annotations;
